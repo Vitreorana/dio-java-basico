04a7d77b4175db91b550635f2798e333e25aa88e
gabriela.marcolan <gabimarcolan@hotmail.com>
2024-05-23 20:54:49 -0300

    Imprimindo números negativos e positivos

--- operadores/operadores/src/App.java
+++ operadores/operadores/src/App.java
@@ -18,6 +18,16 @@ public class App {
 
         concatenacao = "1" + (1+1+1);
         System.out.println(concatenacao);
+
+        int numero = 5;
+        System.out.println(- numero);
+        System.out.println(numero);
+
+        numero = - numero;
+        System.out.println(numero);
+
+        numero = numero * -1;
+        System.out.println(numero);
         
     }
 }
