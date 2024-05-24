66754850868a857b205049c9ce20e3a88dcac556
gabriela.marcolan <gabimarcolan@hotmail.com>
2024-05-23 21:47:40 -0300

    Incrementando numeros e variável boleana

--- operadores/operadores/src/Operadores.java
+++ operadores/operadores/src/Operadores.java
@@ -1,33 +1,17 @@
 public class Operadores {
     public static void main(String[] args) throws Exception {
-        String nomeCompleto = "LINGUAGEM" + "JAVA";
-        System.out.println(nomeCompleto);
-
-        String concatenacao = "?";
-        concatenacao = 1 + 1 + 1 + "1";
-        System.out.println(concatenacao);
-
-        concatenacao = 1 + "1" + 1 + "1";
-        System.out.println(concatenacao);
-
-        concatenacao = 1 + "1" + 1 + 1 ;
-        System.out.println(concatenacao);
-
-        concatenacao = "1" + 1 + 1 + 1 ;
-        System.out.println(concatenacao);
-
-        concatenacao = "1" + (1+1+1);
-        System.out.println(concatenacao);
-
-        int numero = 5;
-        System.out.println(- numero);
+        int numero = 5 ;
+        numero = numero ++;
         System.out.println(numero);
 
-        numero = - numero;
-        System.out.println(numero);
+        boolean variavel = true;
+        System.out.println(! variavel);
 
-        numero = numero * -1;
-        System.out.println(numero);
+        variavel = !variavel;
+        System.out.println(variavel);
+        
+                
+        
         
     }
 }
