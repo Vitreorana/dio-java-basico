ab20a9a16e72e7a427dfe76392bf4f942cb15309
gabriela.marcolan <gabimarcolan@hotmail.com>
2024-05-24 09:02:46 -0300

    Operadores relacionais

--- operadores/operadores/src/Operadores.java
+++ operadores/operadores/src/Operadores.java
@@ -1,17 +1,25 @@
 public class Operadores {
     public static void main(String[] args) throws Exception {
-    int a, b;
-    a = 6;
-    b = 6;
-    
-    String resultado = (a==b) ? "verdadeiro" : "false";
-    System.out.println(resultado);
-
-    
-
-
-                
+    String nomeUm = "GABRIELA";
+    String nomeDois = "GABRIELA";
+    System.out.println(nomeUm == nomeDois);
         
+   
+   
+   
+    int numero1 = 1;
+    int numero2 = 2;
+    boolean simNao = numero1 == numero2;
+    System.out.println("numeroUm e igual a numeroDois?" + simNao);
+
+    simNao = numero1 != numero2;
+    System.out.println("numeroUm e diferente de numeroDois?" + simNao);
+
+    simNao = numero1 > numero2;
+    System.out.println("numeroUm e maior que numeroDois?" + simNao);   
+    
+    if(numero1 < numero2);
+    System.out.println("a nossa condição é verdadeira");
         
     }
 }
