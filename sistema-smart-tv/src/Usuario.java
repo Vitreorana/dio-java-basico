3e2e609348b455bce0a55978b2716463bd664860
gabriela.marcolan <gabimarcolan@hotmail.com>
2024-05-25 08:18:02 -0300

    Definindo o estado atual da SmartTv

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,9 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+      SmartTv smartTv = new SmartTv ();
+        System.out.println("TV Ligada ?" + smartTv.ligada);
+        System.out.println("TV Ligada :" + smartTv.canal);
+        System.out.println("TV Ligada :" + smartTv.volume);
+
+    }
+}
